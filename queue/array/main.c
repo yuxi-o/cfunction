438401a2ab51e20c2972f7c68c030d35a0365842
yuxi-o <88wangqh@163.com>
2020-09-13 22:17:01 +0800

    fix bug for memcpy data and add test main

--- queue/array/main.c
+++ queue/array/main.c
@@ -1,45 +1,63 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include "queue.h"
 
+#define LOOP_COUNT 100
+
+
+squeue_data_t array[] = {
+	{"LOVEA", 6},
+	{"LOVEB", 6},
+	{"LOVEC", 6},
+	{"LOVED", 6},
+	{"LOVEE", 6},
+	{"LOVEF", 6},
+	{"LOVEG", 6},
+	{"LOVEH", 6},
+	{"LOVEI", 6},
+	{"LOVEJ", 6},
+	{"LOVEK", 6},
+	{"LOVEL", 6},
+	{"LOVEM", 6},
+	{"LOVEN", 6},
+};
+
 int main(int argc, char *argv[])
 {
 	squeue_t sq;
-	squeue_data_t data, data1;
+	squeue_data_t data1;
+	int index = 0;
+	int ARRAY_SIZE = sizeof(array) /sizeof(array[0]);
+	int loop = 0;
 
-	squeue_init(&sq, 10);
-	squeue_enqueue_ext(&sq, "LOVEM", 6);
-	squeue_enqueue_ext(&sq, "LOVEN", 6);
 	
-	data.length = 10;  
-	data.pdata = (char *)malloc(5);
-	if(data.pdata == NULL)
+	printf("Array total is %d\n", ARRAY_SIZE);
+	squeue_init(&sq, 10);
+	while(loop++ < LOOP_COUNT)
 	{
-		printf("malloc error");
-		return -1;
-	}
-	memcpy(data.pdata, "LOVEA", 5);
-	squeue_enqueue(&sq, data);
+		printf("---------------------loop %d------------------------\n", loop);
+		while(!squeue_is_full(&sq))
+		{
+			squeue_enqueue_ext(&sq, array[index].pdata, array[index].length);
+			index=(index + 1) % ARRAY_SIZE;
+		}
 
-	memcpy(data.pdata, "LOVEB", 5);
-	squeue_enqueue(&sq, data);
+		squeue_info(&sq);
 
-	memcpy(data.pdata, "LOVEC", 5);
-	squeue_enqueue(&sq, data);
+		while(!squeue_is_empty(&sq))
+		{
+			squeue_dequeue(&sq, &data1);
+			printf("[%d]:%s\n", data1.length, (char *)data1.pdata);
+			squeue_data_destroy(&data1);
+		}
 
-	memcpy(data.pdata, "LOVED", 5);
-	squeue_enqueue(&sq, data);
-
-	squeue_enqueue_ext(&sq, "LOVEE", 6);
-	squeue_info(&sq);
-
-	while(squeue_dequeue(&sq, &data1)== 0)
-	{
-		printf("[%d]:%s\n", data1.length, (char *)data1.pdata);
+		squeue_info(&sq);
+		sleep(1);
 	}
 
-//	squeue_destroy(&sq, squeue_data_destroy);
+	squeue_destroy(&sq, squeue_data_destroy);
 	return 0;
 }
 
